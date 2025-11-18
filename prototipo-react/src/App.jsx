3e158e2f1041fda9d71f4d52b15a45aecaa07ca2
Arthur Maurity <202407173314@alunos.ibmec.edu.br>
2025-11-18 17:04:02 -0300

    correções finais no protótipo React

--- prototipo-react/src/App.jsx
+++ prototipo-react/src/App.jsx
@@ -1,9 +1,32 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './pages/Login';
 import PainelDoAluno from './pages/PainelDoAluno';
 import './App.css';
 
+const RotaProtegida = ({ children }) => {
+  const token = localStorage.getItem('apiToken');
+  if (!token) {
+    return <Navigate to="/" replace />;
+  }
+  return children;
+};
+
 function App() {
   return (
-    <PainelDoAluno />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route 
+          path="/painel-aluno" 
+          element={
+            <RotaProtegida>
+              <PainelDoAluno />
+            </RotaProtegida>
+          } 
+        />
+      </Routes>
+    </Router>
   );
 }
 
